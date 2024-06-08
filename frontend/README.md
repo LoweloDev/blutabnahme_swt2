abad293abad7667013225a6400624503e91bbbc9
Tobias Barthold <tobias-barthold@outlook.de>
2024-06-08 17:35:41 +0200

    scanner working

--- frontend/README.md
+++ frontend/README.md
@@ -25,3 +25,6 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+
+
+ng serve --ssl --ssl-cert certificate.pem --ssl-key privkey.pem --host 192.168.2.101    
--- frontend/src/app/components/scan/scan.component.ts
+++ frontend/src/app/components/scan/scan.component.ts
@@ -1,61 +1,46 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ZXingScannerComponent, ZXingScannerModule} from "@zxing/ngx-scanner";
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {BarcodeFormat} from "@zxing/browser";
-import jsQR from "jsqr";
+
+import {AsyncPipe, JsonPipe, NgIf} from "@angular/common";
+import {ZXingScannerComponent, ZXingScannerModule} from "@zxing/ngx-scanner";
 
 @Component({
   selector: 'app-scan',
   standalone: true,
   imports: [
+    JsonPipe,
+    AsyncPipe,
+    NgIf,
     ZXingScannerModule
   ],
   templateUrl: './scan.component.html',
   styleUrl: './scan.component.css'
 })
-export class ScanComponent implements OnInit{
-  @ViewChild('video', { static: true }) video: ElementRef<HTMLVideoElement>;
-  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
-  videoElement: HTMLVideoElement;
-  canvasElement: HTMLCanvasElement;
-  canvasContext: CanvasRenderingContext2D;
+export class ScanComponent implements OnInit, AfterViewInit {
+  @ViewChild(ZXingScannerComponent, { static: false })
+  public scanner?: ZXingScannerComponent;
+  allowedFormats = [ BarcodeFormat.QR_CODE, BarcodeFormat.EAN_13, BarcodeFormat.CODE_128, BarcodeFormat.DATA_MATRIX ];
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log("HELLO TEST");
-    this.videoElement = this.video.nativeElement;
-    this.canvasElement = this.canvas.nativeElement;
-    this.canvasContext = this.canvasElement.getContext('2d') as CanvasRenderingContext2D;
-
-    this.startCamera();
+    window.alert("TEST");
   }
 
-  startCamera() {
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
-      .then(stream => {
-        this.videoElement.srcObject = stream;
-        this.videoElement.setAttribute('playsinline', 'true'); // required to tell iOS safari we don't want fullscreen
-        this.videoElement.play();
-        requestAnimationFrame(this.tick.bind(this));
-      })
-      .catch(err => {
-        console.error(err);
-      });
+  ngAfterViewInit(): void {
+    this.scanAllowedCodes();
   }
 
-  tick() {
-    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
-      this.canvasElement.height = this.videoElement.videoHeight;
-      this.canvasElement.width = this.videoElement.videoWidth;
-      this.canvasContext.drawImage(this.videoElement, 0, 0, this.canvasElement.width, this.canvasElement.height);
-      const imageData = this.canvasContext.getImageData(0, 0, this.canvasElement.width, this.canvasElement.height);
-      const code = jsQR(imageData.data, imageData.width, imageData.height);
-      if (code) {
-        console.log('Scanned code:', code.data);
-        window.alert(`Scanned code:${code.data}`);
-        // Optionally, you can stop the camera or perform any other action here
-      }
+  public scanAllowedCodes() {
+    if (!this.scanner) {
+      console.error("Scanner not found");
+      return;
     }
-    requestAnimationFrame(this.tick.bind(this));
+
+    this.scanner.autofocusEnabled = true;
+
+    this.scanner.scanSuccess.subscribe((result: string) => {
+      window.alert('Barcode result: ' + result);
+    });
   }
 }
