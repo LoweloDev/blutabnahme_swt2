40e2962b5fc22f7adeee0f2ed0fa5360ccc0162a
Tobias Barthold <tobias-barthold@outlook.de>
2024-07-22 14:28:05 +0200

    remove comments

--- backend/src/modules/blutabnahme/blutabnahme.subscriber.ts
+++ backend/src/modules/blutabnahme/blutabnahme.subscriber.ts
@@ -5,36 +5,45 @@ import {
   UpdateEvent,
 } from "typeorm";
 import { Blutabnahme } from "./entities/blutabnahme.entity";
+import { MqttService } from "../../shared/services/mqtt/mqtt.service";
+import { MqttEvent } from "../../shared/services/mqtt/mqtt-event";
 
 @EventSubscriber()
 export class BlutabnahmeSubscriber
   implements EntitySubscriberInterface<Blutabnahme>
 {
-  /**
-   * Indicates that this subscriber only listen to User events.
-   */
   listenTo() {
     return Blutabnahme;
   }
 
-  /**
-   * Called after entity is inserted.
-   */
-  afterInsert(event: InsertEvent<Blutabnahme>) {
+  async afterInsert(event: InsertEvent<Blutabnahme>) {
+    const mqtt = new MqttService();
     console.log(`Inserted user with id ${event.entity.id}`);
+    await mqtt.publish(
+      "blutabnahme",
+      JSON.stringify(new MqttEvent("insert", event.entity)),
+    );
+
+    mqtt.close();
   }
 
-  /**
-   * Called after entity is updated.
-   */
-  afterUpdate(event: UpdateEvent<Blutabnahme>) {
+  async afterUpdate(event: UpdateEvent<Blutabnahme>) {
+    const mqtt = new MqttService();
     console.log(`Updated user with id ${event.entity.id}`);
+    await mqtt.publish(
+      "blutabnahme",
+      JSON.stringify(new MqttEvent("update", event.entity)),
+    );
+    mqtt.close();
   }
 
-  /**
-   * Called after entity is removed.
-   */
-  afterRemove(event: any) {
+  async afterRemove(event: any) {
+    const mqtt = new MqttService();
     console.log(`Removed user with id ${event.entityId}`);
+    await mqtt.publish(
+      "blutabnahme",
+      JSON.stringify(new MqttEvent("remove", event.entityId)),
+    );
+    mqtt.close();
   }
 }
